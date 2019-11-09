7d99beffaa680299137f59087ed697914795072d
cullenMichael <cullenm9@tcd.ie>
2019-11-09 22:05:10 +0000

    PEP-8 Standard, Removed duplicate rows, removed n/a year rows, dropped unused cols

--- pipeline-example.py
+++ pipeline-example.py
@@ -2,16 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from catboost import CatBoostRegressor
-from xgboost import XGBRegressor
 from category_encoders import *
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import (FunctionTransformer, MinMaxScaler,
                                    OrdinalEncoder, StandardScaler)
 from sklearn.utils import shuffle
+
+# from xgboost import XGBRegressor
 
 
 def imputation(train):
@@ -19,14 +21,15 @@ def imputation(train):
     # train['Housing Situation'].replace(0, np.nan, inplace= True)
     # train['Housing Situation'].replace('nA', np.nan, inplace= True)
 
-    train.pop('Instance')
+    # train.pop('Instance')
 
     ord_si_step = ('si', SimpleImputer(strategy='constant',
                                        fill_value='MISSING'))
     ord_oe_step = ('oe', OrdinalEncoder())
     ord_steps = [ord_si_step, ord_oe_step]
     ord_pipe = Pipeline(ord_steps)
-    ord_cols = ['Satisfation with employer']#,'University Degree' 'Housing Situation']
+    # ord_cols = ['Satisfation with employer']#,'University Degree' 'Housing Situation']
+    ord_cols = []
 
     cat_si_step = ('si', SimpleImputer(strategy='constant',
                                        fill_value='MISSING'))
@@ -35,7 +38,7 @@ def imputation(train):
     cat_pipe = Pipeline(cat_steps)
     cat_cols = ['Gender', 'Country', 'Profession', 'Housing Situation'] # 'Hair Color'
 
-    num_cols = ['Year of Record', 'Age', 'Body Height [cm]', 'Crime Level in the City of Employement', 'Work Experience in Current Job [years]']
+    num_cols = ['Year of Record', 'Age', 'Body Height [cm]', 'Work Experience in Current Job [years]']
     num_si_step = ('si', SimpleImputer(strategy='median'))
     num_ss_step = ('ss', StandardScaler())
     num_steps = [num_si_step, num_ss_step]
@@ -56,81 +59,84 @@ def Scaler(X_train):
     scaler.fit(X_train)
     return scaler.transform(X_train)
 
+
 def changeSizeOfCity(dataset):
-    #Possibly refine number for this
+    # Possibly refine number for this
     dataset['Small City'] = dataset['Size of City'] < 3000;
-    #dataset.pop('Size of City')
+    # dataset.pop('Size of City')
     return dataset
 
+
 def processAdditionToSalary(dataset):
-    #Get rid of "EUR" and change to float
+    # Get rid of "EUR" and change to float
     dataset['Yearly Income in addition to Salary (e.g. Rental Income)'] = dataset['Yearly Income in addition to Salary (e.g. Rental Income)'].map(lambda x: x.rstrip(' EUR'))
     dataset['Yearly Income in addition to Salary (e.g. Rental Income)'].astype('float')
     return dataset
 
-#Might scrap this - no improvement in MAE
+
+# Might scrap this - no improvement in MAE
 def degree(dataset):
     dataset['Has Degree'] = dataset['University Degree'].str.contains(pat = 'Bachelor|PhD|Master')
-    #dataset.pop('University Degree')
+    # dataset.pop('University Degree')
     return dataset
 
-def HousingSituation(dataset,isTestData):
 
-        # replace 0 and nA with common aribale "Unknown"
-        dataset["Housing Situation"]= dataset["Housing Situation"].replace(0, "Unknown")
-        dataset["Housing Situation"]= dataset["Housing Situation"].replace("0", "Unknown")
-        dataset["Housing Situation"]= dataset["Housing Situation"].replace("nA", "Unknown")
+def HousingSituation(dataset, isTestData):
+    # replace 0 and nA with common aribale "Unknown"
+    dataset["Housing Situation"] = dataset["Housing Situation"].replace(0, "Unknown")
+    dataset["Housing Situation"] = dataset["Housing Situation"].replace("0", "Unknown")
+    dataset["Housing Situation"] = dataset["Housing Situation"].replace("nA", "Unknown")
 
-        #if training data then drop columns otherwise leave them in
-        if(isTestData!=True):
-            for (i, row) in dataset.iterrows():
-                if row["Total Yearly Income [EUR]"] > 400000:
-                    if row["Housing Situation"] in ("Castle", "Unknown", "Large House"):
-                        dataset.drop(i,axis=0)
+    # if training data then drop columns otherwise leave them in
+    if(isTestData is not True):
+        for (i, row) in dataset.iterrows():
+            if row["Total Yearly Income [EUR]"] > 400000:
+                if row["Housing Situation"] in ("Castle", "Unknown", "Large House"):
+                    dataset = dataset.drop(i, axis=0)
 
-        #Shuffle dataset to ensure forward propagation
-        dataset=shuffle(dataset)
-        #Foward fill the the data
-        dataset=dataset.replace({'Housing Situation': {"Unknown": np.nan}}).ffill()
-        # resort the data to original ordering
-        dataset=dataset.sort_values(by=['Instance'])
+    # Shuffle dataset to ensure forward propagation
+    dataset = shuffle(dataset)
+    # Foward fill the the data
+    dataset = dataset.replace({'Housing Situation': {"Unknown": np.nan}}).ffill()
+    # resort the data to original ordering
+    dataset = dataset.sort_values(by=['Instance'])
 
-        return dataset
-
-def genderCleaning(dataset,isTestData):
-
-        # replace 0 and nA with common aribale "Unknown"
-        dataset["Gender"]= dataset["Gender"].replace("f", "female")
-        dataset["Gender"]= dataset["Gender"].replace("0", "unknown")
-        dataset["Gender"]= dataset["Gender"].replace(0, "unknown")
-        dataset["Gender"]= dataset["Gender"].replace(np.NaN, "unknown")
+    return dataset
 
 
-        #if training data then drop columns otherwise leave them in
-        if(isTestData!=True):
-            for (i, row) in dataset.iterrows():
-                if row["Gender"] == "unknown":
-                    if row["Total Yearly Income [EUR]"] > 1500000:
-                        dataset = dataset.drop([dataset.index[i]])
+def genderCleaning(dataset, isTestData):
+    # replace 0 and nA with common aribale "Unknown"
+    dataset["Gender"] = dataset["Gender"].replace("f", "female")
+    dataset["Gender"] = dataset["Gender"].replace("0", "unknown")
+    dataset["Gender"] = dataset["Gender"].replace(0, "unknown")
+    dataset["Gender"] = dataset["Gender"].replace(np.NaN, "unknown")
 
-        #Shuffle dataset to ensure forward propagation
-        dataset=shuffle(dataset)
-        #Foward fill the the data
-        dataset=dataset.replace({"Gender": {"unknown": np.nan}}).ffill()
-        # resort the data to original ordering
-        dataset=dataset.sort_values(by=["Instance"])
+    # if training data then drop columns otherwise leave them in
+    if(isTestData is not True):
+        for (i, row) in dataset.iterrows():
+            if row["Gender"] == "unknown":
+                if row["Total Yearly Income [EUR]"] > 1500000:
+                    dataset = dataset.drop(i, axis=0)
 
-        return dataset
+    # Shuffle dataset to ensure forward propagation
+    dataset = shuffle(dataset)
+    # Foward fill the the data
+    dataset = dataset.replace({"Gender": {"unknown": np.nan}}).ffill()
+    # resort the data to original ordering
+    dataset = dataset.sort_values(by=["Instance"])
+
+    return dataset
 
 
-#No Missing values in Body Height so no need for Backward propagation
+# No Missing values in Body Height so no need for Backward propagation
 def bodyHeight(dataset):
-        std = dataset['Body Height [cm]'].std(axis = 0)
-        mean = dataset['Body Height [cm]'].mean()
-        dataset['1StdBH'] = ((dataset['Body Height [cm]'] >= (mean - std)) & (dataset['Body Height [cm]'] <= (mean + std)))
-        dataset['Outside 1 Std'] = ((dataset['Body Height [cm]'] <= (mean - std)) | (dataset['Body Height [cm]'] >= (mean + std)))
-        #dataset.pop('Body Height [cm]')
-        return dataset
+    std = dataset['Body Height [cm]'].std(axis = 0)
+    mean = dataset['Body Height [cm]'].mean()
+    dataset['1StdBH'] = ((dataset['Body Height [cm]'] >= (mean - std)) & (dataset['Body Height [cm]'] <= (mean + std)))
+    dataset['Outside 1 Std'] = ((dataset['Body Height [cm]'] <= (mean - std)) | (dataset['Body Height [cm]'] >= (mean + std)))
+    # dataset.pop('Body Height [cm]')
+    return dataset
+
 
 def main():
     # Loading in training dataset using pandas
@@ -139,16 +145,25 @@ def main():
     # Split dataset into target(y) and predictor variables(train)
     train = dataset
 
+    # When data gets messed up
+    train = train[:1044560]
+    # Remove Duplicates
+    train = train.drop_duplicates(subset='Instance', keep='first',
+                                  inplace=False)
+
     # Fileter housing data
-    train = genderCleaning(train,False)
-    train = HousingSituation(train,False)
+    train = genderCleaning(train, False)
+    train = HousingSituation(train, False)
 
-    #Put Income on Log Scale
+    # Put Income on Log Scale
     train["Total Yearly Income [EUR]"] = train["Total Yearly Income [EUR]"].apply(np.log)
-    y = train.pop("Total Yearly Income [EUR]").values
+    y = train['Total Yearly Income [EUR]'].values
 
-    train.pop('Hair Color')
-    train.pop('Wears Glasses')
+    # Dropped columns
+    train = train.drop(columns=['Instance', 'Hair Color',
+                                'Crime Level in the City of Employement',
+                                'Satisfation with employer',
+                                'Total Yearly Income [EUR]'])
     train = bodyHeight(train)
     train = changeSizeOfCity(train)
     train = processAdditionToSalary(train)
@@ -156,34 +171,33 @@ def main():
     ct = imputation(train)
     regressor = CatBoostRegressor()
 
-
     X_train, X_test, y_train, y_test = train_test_split(
         train, y, test_size=0.2)
 
+    print 'Fitting'
     ml_pipe = Pipeline([
         ('transform', ct),
         ('regressor', regressor)])
     ml_pipe.fit(X_train, y_train)
-
+    print 'Predicting'
     y_pred = ml_pipe.predict(X_test)
     print('MAE is: {}'.format(mean_absolute_error(np.exp(y_test), np.exp(y_pred))))
 
     test_dataset = pd.read_csv(
         'tcd-ml-1920-group-income-test.csv')
 
-    #Split into target and predictor variables
+    # Split into target and predictor variables
     predict_X = test_dataset
     predict_X = genderCleaning(predict_X, True)
     predict_X = HousingSituation(predict_X,True)
-    predict_X = predict_X.drop("Instance", axis='columns')
     predict_y = predict_X.pop("Total Yearly Income [EUR]").values
-    predict_X.pop('Hair Color')
-    predict_X.pop('Wears Glasses')
+    predict_X = predict_X.drop(columns=['Instance', 'Hair Color',
+                                        'Crime Level in the City of Employement',
+                                        'Satisfation with employer'])
     predict_X = bodyHeight(predict_X)
     predict_X = changeSizeOfCity(predict_X)
     predict_X = processAdditionToSalary(predict_X)
     predict_X = degree(predict_X)
-
 
     predict_X['Work Experience in Current Job [years]'].replace('#NUM!', np.nan, inplace = True)
 
