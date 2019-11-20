Commented, minor scaling and year fix

regressionWithBestModelParameter.py
@@ -17,8 +17,12 @@ from sklearn.preprocessing import (FunctionTransformer, MinMaxScaler,
                                    OneHotEncoder, OrdinalEncoder,
                                    StandardScaler)
 from sklearn.utils import shuffle
+import math
 
 
+'''
+    Adds new column for Cities less than 3000
+'''
 def changeSizeOfCity(dataset):
     dataset['Small City'] = dataset['Size of City'] < 3000
     dataset['Small City'] = np.log(dataset['Size of City'])
@@ -26,23 +30,31 @@ def changeSizeOfCity(dataset):
     return dataset
 
 
+'''
+    Gets rid of '#NUM!', converts to float array and scales around the mean
+'''
 def work_experience(dataset):
-    # work_mean = dataset['Work Experience in Current Job [years]'].mean()
     dataset['Work Experience in Current Job [years]'] = dataset['Work Experience in Current Job [years]'].replace('#NUM!', 1)
     dataset['Work Experience in Current Job [years]'] = np.array(dataset['Work Experience in Current Job [years]'], dtype=np.float32)
-    dataset['Work Experience in Current Job [years]'] = dataset['Work Experience in Current Job [years]'].replace(0, 1)
-    # dataset['Work Experience in Current Job [years]'] = np.log(dataset['Work Experience in Current Job [years]'])
+    mean = dataset['Work Experience in Current Job [years]'].mean()
+    dataset['Work Experience in Current Job [years]'] = dataset['Work Experience in Current Job [years]'] - mean
     return dataset
 
 
+'''
+    Strips 'EUR', converts to float and scales around the log
+'''
 def processAdditionToSalary(dataset):
     dataset['Yearly Income in addition to Salary (e.g. Rental Income)'] = dataset['Yearly Income in addition to Salary (e.g. Rental Income)'].map(lambda x: x.rstrip(' EUR'))
     dataset['Yearly Income in addition to Salary (e.g. Rental Income)'] = dataset['Yearly Income in addition to Salary (e.g. Rental Income)'].astype('float')
     dataset['Yearly Income in addition to Salary (e.g. Rental Income)'] = dataset['Yearly Income in addition to Salary (e.g. Rental Income)'].replace(0, 1)
-    # dataset['Yearly Income in addition to Salary (e.g. Rental Income)'] = np.log(dataset['Yearly Income in addition to Salary (e.g. Rental Income)'])
+    dataset['Yearly Income in addition to Salary (e.g. Rental Income)'] = np.log(dataset['Yearly Income in addition to Salary (e.g. Rental Income)'])
     return dataset
 
 
+'''
+    Replaces nan and 0 values with MISSING
+'''
 def degree(dataset):
     dataset["University Degree"] = dataset["University Degree"].replace(np.nan, "MISSING")
     dataset["University Degree"] = dataset["University Degree"].replace(0, "MISSING")
@@ -50,15 +62,34 @@ def degree(dataset):
     return dataset
 
 
+'''
+    Replaces f with Female, Replaces 0 and nan with unknown, fills unknown values based on their Body Height
+'''
 def genderCleaning(dataset):
     dataset["Gender"] = dataset["Gender"].replace("f", "female")
     dataset["Gender"] = dataset["Gender"].replace("0", "unknown")
     dataset["Gender"] = dataset["Gender"].replace(0, "unknown")
-    dataset['Gender'] = dataset['Gender'].fillna(method='bfill')
-    # dataset["Gender"] = dataset["Gender"].replace(np.NaN, "unknown")
+    dataset["Gender"] = dataset["Gender"].replace(np.NaN, "unknown")
+    #dataset['Gender'] = dataset['Gender'].fillna(method='bfill')
+
+    gender_height = dataset.groupby('Gender').median()[['Body Height [cm]']]
+    print(gender_height)
+    male= gender_height.loc["male"]["Body Height [cm]"]
+    female= gender_height.loc["female"]["Body Height [cm]"]
+    for (i, row) in dataset.iterrows():
+        if row["Gender"] == "unknown":
+            if abs(male-row["Body Height [cm]"]) < abs(female - row["Body Height [cm]"]):
+                dataset.set_value(i,"Gender", "male")
+            else:
+                dataset.set_value(i,"Gender", "female")
+    print("Finished Gender")
+
     return dataset
 
 
+'''
+    Backwards Fill the missing values. Ordinal encode the data and convert to int
+'''
 def satisfaction(dataset):
     dataset['Satisfation with employer'] = dataset['Satisfation with employer'].fillna(method='bfill')
     dataset['Satisfation with employer'] = dataset['Satisfation with employer'].replace('Unhappy', 0)
@@ -68,90 +99,63 @@ def satisfaction(dataset):
     dataset['Satisfation with employer'] = dataset['Satisfation with employer'].astype('int')
     return dataset
 
-
-# def age(dataset):
-#     dataset['Age'] = np.log(dataset['Age'])
-#     return dataset
-
-
+'''
+    In testing data, fill the years based on the Housing Situation (High correlation with Year)
+'''
 def year(dataset):
-    dataset['Year of Record'] = dataset['Year of Record'].fillna(method='bfill')
-    # dataset["Year of Record"] = dataset["Year of Record"].replace(np.nan, "MISSING")
+    #dataset['Year of Record'] = dataset['Year of Record'].fillna(method='bfill')
+    dataset["Housing Situation"] = dataset["Housing Situation"].replace("nA", "Unknown")
+    housing = dataset.groupby('Housing Situation').mean()[['Year of Record']]
+
+    for (i, row) in dataset.iterrows():
+        if math.isnan(row["Year of Record"]):
+            print("here")
+            dataset.set_value(i,"Year of Record", int(housing.loc[row["Housing Situation"]]["Year of Record"]))
     return dataset
 
 
+'''
+    Scale Body Height around the mean
+'''
 def bodyHeight(dataset):
+    mean = dataset['Body Height [cm]'].mean()
+    dataset['Body Height [cm]'] = dataset['Body Height [cm]'] - mean
     return dataset
 
 
+'''
+    Backwards Fill Profession and only take the first 5 characters for each row.
+'''
 def profession(dataset):
-    # dataset['Profession'].fillna('none', inplace=True)
     dataset['Profession'] = dataset['Profession'].fillna(method='bfill')
+    dataset["Profession"] = dataset["Profession"].astype(str).str[:5]
     return dataset
 
 
-def housing(dataset):
-    dataset["Housing Situation"] = dataset["Housing Situation"].replace(0, 0)
-    dataset["Housing Situation"] = dataset["Housing Situation"].replace("0", 0)
-    dataset["Housing Situation"] = dataset["Housing Situation"].replace("nA", np.nan)
-    dataset['Housing Situation'] = dataset['Housing Situation'].fillna(method='bfill')
-
-
-
-    # dataset["Housing Situation"] = dataset["Housing Situation"].replace('Castle', 0)
-    # dataset["Housing Situation"] = dataset["Housing Situation"].replace('Large House', 0)
-    # dataset["Housing Situation"] = dataset["Housing Situation"].replace('Medium House', 0)
-    # dataset["Housing Situation"] = dataset["Housing Situation"].replace('Small House', 0)
-    # dataset["Housing Situation"] = dataset["Housing Situation"].replace('Large Apartment', 3)
-    # dataset["Housing Situation"] = dataset["Housing Situation"].replace('Medium Apartment', 2)
-    # dataset["Housing Situation"] = dataset["Housing Situation"].replace('Small Apartment', 1)
-    # dataset["Housing Situation"] = dataset["Housing Situation"].astype('int')
-
-    # arr = np.zeros(len(dataset['Housing Situation']))
-    # arr = arr[dataset["Housing Situation"] == 'Large Apartment']=1
-    # arr = arr[dataset["Housing Situation"] == 'Medium Apartment']=2
-    # arr = arr[dataset["Housing Situation"] == 'Small Apartment']=3
-
-    # dataset.pop('Housing Situation')
-
-    return dataset
-
-
+'''
+    Scales Crime Level around the mean
+'''
 def crime(dataset):
-    dataset['C']
-
-
-def hair(dataset):
-    dataset['Hair Color'] = dataset['Hair Color'].replace(0, "Unknown")
-    dataset['Hair Color'] = dataset['Hair Color'].replace('0', "Unknown")
-    dataset['Hair Color'] = dataset['Hair Color'].replace(np.nan, "Unknown")
+    mean = dataset['Crime Level in the City of Employement'].mean()
+    dataset['Crime Level in the City of Employement'] = dataset['Crime Level in the City of Employement'] - mean
     return dataset
 
 
 def main():
 
     dataset = pd.read_csv('tcd-ml-1920-group-income-train.csv')
-
-
-
     train = dataset.copy()
-
     train = train[:1044560]
-
-    train = train.drop_duplicates(subset='Instance', keep='first',
-                                  inplace=False)
-
-    # train = train[train['Year of Record'] > 1980]
-
+    train = train.drop_duplicates(subset='Instance', keep='first',inplace=False)
+    train = train.drop(columns = ['Instance'])
+    train = train.drop_duplicates(inplace = False)
     y = np.log(train['Total Yearly Income [EUR]'])
 
-    train = train.drop(columns=['Instance',
-                                'Total Yearly Income [EUR]',
+    train = train.drop(columns=['Total Yearly Income [EUR]',
                                 'Hair Color',
                                 'Housing Situation',
                                 'Wears Glasses',
                                 ])
-
     train = bodyHeight(train)
     train = changeSizeOfCity(train)
     train = degree(train)
@@ -160,18 +164,15 @@ def main():
     train = satisfaction(train)
     train = work_experience(train)
     train = processAdditionToSalary(train)
-    # train = housing(train)
+    train = crime(train)
 
-    # import seaborn as sns
-    # g = sns.pairplot(train[['Age', 'Housing Situation', 'Year of Record', 'Satisfation with employer', 'Yearly Income in addition to Salary (e.g. Rental Income)', 'Work Experience in Current Job [years]']])
-    # plt.show()
-
+    #Encode using get dummies
     print ("Start Dummies")
     train = pd.get_dummies(train, columns=['Gender',
                                            'Country',
                                            'University Degree',
-                                           'Profession'], drop_first=False)
-    print "End Dummies"
+                                           'Profession'], drop_first=True)
+    print ("End Dummies")
 
     regressor = CatBoostRegressor(od_type='IncToDec')
 
@@ -185,14 +186,13 @@ def main():
                         y_val)
 
     print ('Fitting')
-
-    parameters = {'depth'         : sp_randInt(4, 11),
+    parameters = {'depth'         : sp_randInt(3,4),
                   'learning_rate' : sp_randFloat(),
-                  'iterations'    : sp_randInt(700, 800)
+                  'iterations'    : sp_randInt(200, 300)
                  }
 
     randm = RandomizedSearchCV(estimator=regressor, param_distributions = parameters,
-                               cv = 4, n_iter = 10, n_jobs=9)
+                               cv = 4, n_iter = 10, n_jobs=5)
     randm.fit(X_train, y_train)
     # Results from Random Search
     print("\n========================================================")
@@ -205,20 +205,12 @@ def main():
 
     print("\n The best parameters across ALL searched params:\n",randm.best_params_)
 
-    # print("\n ========================================================")
-
     regressor = CatBoostRegressor(iterations=randm.best_params_['iterations'],
                                   learning_rate=randm.best_params_['learning_rate'],
                                   depth=randm.best_params_['depth'],
                                   od_type='IncToDec',
                                   use_best_model=True)
 
-
-    # print ('Fitting')
-    # regressor.fit(X_train, y_train)
-    # print ('Predicting Locally')
-    # y_pred = regressor.predict(X_test)
-    # print('MAE is: {}'.format(mean_absolute_error(np.exp(y_test), np.exp(y_pred))))
 
     test_dataset = pd.read_csv(
         'tcd-ml-1920-group-income-test.csv')
@@ -227,6 +219,7 @@ def main():
     X_train = train
     y_train = y
 
+    predict_X = year(predict_X)
     predict_X = predict_X.drop(columns=['Instance',
                                 'Hair Color',
                                 'Total Yearly Income [EUR]',
@@ -241,16 +234,17 @@ def main():
     predict_X = profession(predict_X)
     predict_X = work_experience(predict_X)
     predict_X = processAdditionToSalary(predict_X)
-    predict_X = year(predict_X)
+
     predict_X = satisfaction(predict_X)
+    predict_X = crime(predict_X)
 
     predict_X = pd.get_dummies(predict_X, columns=['Gender', 'Profession',
                                             'Country',
-                                            'University Degree'], drop_first=False)
+                                            'University Degree'], drop_first=True)
 
     X_train, predict_X = train.align(predict_X , join='outer', axis=1, fill_value=0)
 
-    print 'Fitting Test Data'
+    print ('Fitting Test Data')
     regressor.fit(X_training, y_training,eval_set=eval_dataset)
 
     pred2 = regressor.predict(predict_X)
@@ -266,7 +260,6 @@ def main():
 
     y_pred = regressor.predict(X_test)
     print('MAE is: {}'.format(mean_absolute_error(np.exp(y_test), np.exp(y_pred))))
-
 
 
 if __name__ == "__main__":